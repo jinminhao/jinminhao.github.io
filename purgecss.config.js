f86bf66c21095fc9df242f631239cebd057ab2b2
Minhao Jin <minhaoj@princeton.edu>
2026-05-11 22:18:43 -0400

    fix: safelist data-theme selectors in PurgeCSS to preserve dark mode

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,17 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  safelist: {
+    // Preserve classes toggled dynamically by JS (not in static HTML)
+    standard: [
+      "fa-sun",
+      "fa-moon",
+      "transition",
+      "table-dark",
+      "repo-img-light",
+      "repo-img-dark",
+    ],
+    // Preserve any selector that contains data-theme (dark mode blocks)
+    greedy: [/data-theme/],
+  },
 };
